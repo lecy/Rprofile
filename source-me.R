c8fc5c1dbbab5281d92ed0f5f3fdb99456efaecc
lecy <jdlecy@gmail.com>
2017-02-20 13:39:37 -0500

    Create source-me.R

---
+++ source-me.R
@@ -0,0 +1,94 @@
+
+# do not convert strings to factors automatically
+options( stringsAsFactors=FALSE )
+
+# do not use scientific convention except for large numbers
+options( scipen=8 )
+
+
+
+
+# custom plot function
+
+jplot <- function( x1, x2, lab1="", lab2="", draw.line=T, ... )
+{
+
+	plot( x1, x2,
+	      pch=19, 
+	      col=gray(0.6, alpha = 0.2), 
+	      cex=3.5,  
+	      bty = "n",
+	      xlab=lab1, 
+	      ylab=lab2, cex.lab=1.5,
+        ... )
+
+	if( draw.line==T ){ lines( lowess(x2~x1), col="red", lwd=3 ) }
+
+}
+
+
+
+
+
+# nice color functions from: john baums
+# https://gist.github.com/johnbaums/45b49da5e260a9fc1cd7
+# 
+swatch <- function(x) {
+  # x: a vector of colours (hex, numeric, or string)
+  par(mai=c(0.2, max(strwidth(x, "inch") + 0.4, na.rm = TRUE), 0.2, 0.4))
+  barplot(rep(1, length(x)), col=rev(x), space = 0.1, axes=FALSE, 
+          names.arg=rev(x), cex.names=0.8, horiz=T, las=1)  
+}
+ 
+# Example:
+# swatch(colours()[1:10])
+# swatch(iwanthue(5))
+# swatch(1:4)
+
+iwanthue <- function(n, hmin=0, hmax=360, cmin=0, cmax=180, lmin=0, lmax=100, 
+                     plot=FALSE, random=FALSE) {
+  # Presently doesn't allow hmax > hmin (H is circular)
+  # n: number of colours
+  # hmin: lower bound of hue (0-360)
+  # hmax: upper bound of hue (0-360)
+  # cmin: lower bound of chroma (0-180)
+  # cmax: upper bound of chroma (0-180)
+  # lmin: lower bound of luminance (0-100)
+  # lmax: upper bound of luminance (0-100)
+  # plot: plot a colour swatch?
+  # random: should clustering be random? (if FALSE, seed will be set to 1,
+  #         and the RNG state will be restored on exit.) 
+  require(colorspace)
+  stopifnot(hmin >= 0, cmin >= 0, lmin >= 0, 
+            hmax <= 360, cmax <= 180, lmax <= 100, 
+            hmin <= hmax, cmin <= cmax, lmin <= lmax,
+            n > 0)
+  if(!random) {
+    if (exists(".Random.seed", .GlobalEnv)) {
+      old_seed <- .GlobalEnv$.Random.seed
+      on.exit(.GlobalEnv$.Random.seed <- old_seed)
+    } else {
+      on.exit(rm(".Random.seed", envir = .GlobalEnv))
+    }
+    set.seed(1)
+  }
+  lab <- LAB(as.matrix(expand.grid(seq(0, 100, 1), 
+                                   seq(-100, 100, 5), 
+                                   seq(-110, 100, 5))))
+  if (any((hmin != 0 || cmin != 0 || lmin != 0 ||
+           hmax != 360 || cmax != 180 || lmax != 100))) {
+    hcl <- as(lab, 'polarLUV')
+    hcl_coords <- coords(hcl)
+    hcl <- hcl[which(hcl_coords[, 'H'] <= hmax & hcl_coords[, 'H'] >= hmin &
+                       hcl_coords[, 'C'] <= cmax & hcl_coords[, 'C'] >= cmin & 
+                       hcl_coords[, 'L'] <= lmax & hcl_coords[, 'L'] >= lmin), ]
+    #hcl <- hcl[-which(is.na(coords(hcl)[, 2]))]
+    lab <- as(hcl, 'LAB')    
+  }
+  lab <- lab[which(!is.na(hex(lab))), ]
+  clus <- kmeans(coords(lab), n, iter.max=50)
+  if (isTRUE(plot)) {
+    swatch(hex(LAB(clus$centers)))
+  }
+  hex(LAB(clus$centers))
+}
